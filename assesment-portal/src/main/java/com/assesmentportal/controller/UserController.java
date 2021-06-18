a96a0f30bbd99d51a9de2293222e3abd53b6a3f8
mwaghmode-dev <mwaghmode31@gmail.com>
2021-06-18 12:01:38 +0530

    Added structure

---
+++ assesment-portal/src/main/java/com/assesmentportal/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.assesmentportal.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ assesment-portal/src/main/java/com/assesmentportal/services/UserService.java
@@ -0,0 +1,7 @@
+package com.assesmentportal.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
