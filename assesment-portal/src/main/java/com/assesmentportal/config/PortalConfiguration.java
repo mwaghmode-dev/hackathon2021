29d9e802d11e3a3fe32aece0f10955aefc870376
Lokesh Kumar <70200254+loks247@users.noreply.github.com>
2021-06-18 20:27:40 +0530

    Project security (#6)
    * correcting required dependency for security
    
    * setting configuration

---
+++ assesment-portal/src/main/java/com/assesmentportal/config/PortalConfiguration.java
@@ -0,0 +1,41 @@
+package com.assesmentportal.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class PortalConfiguration {
+
+	@Bean
+	public DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http
+			.cors().configurationSource(corsConfigurationSource()).and()
+			.csrf().disable()
+			.formLogin().disable()
+			.httpBasic().disable()
+			.authorizeRequests().anyRequest().permitAll();
+		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Access-Control-Allow-Origin"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/api/**", configuration);
+		return source;
+	}
+
+}
