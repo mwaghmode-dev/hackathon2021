c9d4d90ec47995ca09822a2d903b978c602b8e9d
mukund.waghmode <mukund.waghmode@appdirect.com>
2021-06-18 11:23:45 +0530

    setup added

---
+++ assesment-portal/src/main/java/com/assesmentportal/AssesmentPortalApplication.java
@@ -0,0 +1,11 @@
+package com.assesmentportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssesmentPortalApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AssesmentPortalApplication.class, args);
+	}
+}
