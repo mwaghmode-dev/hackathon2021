27110247658f8fa9aeef8a2c68a6ed6186ab46c5
Mukund Waghmode <55744695+mukundwaghmode@users.noreply.github.com>
2021-06-19 20:52:18 +0530

    userController added (#7)

--- assesment-portal/src/main/java/com/assesmentportal/controller/UserController.java
+++ assesment-portal/src/main/java/com/assesmentportal/controller/UserController.java
@@ -1,7 +1,44 @@
 package com.assesmentportal.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.assesmentportal.model.User;
+import com.assesmentportal.services.UserService;
+
 @RestController
+@RequestMapping(value = "/api/assesmentportal", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/users/{id}")
+	public User getUser(@PathVariable String id){
+		return userService.getUser(id);
+	}
+
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		return userService.getAllUsers();
+	}
+
+	@PostMapping("/users")
+	public boolean addUser(@RequestBody User user){
+		return userService.addUser(user);
+	}
+
+	@PutMapping("/users")
+	public boolean updateUser(@RequestBody User user){
+		return userService.updateUser(user);
+	}
 }
--- assesment-portal/src/main/java/com/assesmentportal/services/UserService.java
+++ assesment-portal/src/main/java/com/assesmentportal/services/UserService.java
@@ -1,7 +1,34 @@
 package com.assesmentportal.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.assesmentportal.model.User;
+import com.assesmentportal.repositories.UserRepository;
 
 @Service
 public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public User getUser(String id) {
+		return userRepository.findOne(id);
+	}
+
+	public boolean addUser(User user) {
+		userRepository.save(user);
+		return true;
+	}
+
+	public List<User> getAllUsers() {
+		return (List<User>)userRepository.findAll();
+	}
+
+	public boolean updateUser(User user) {
+		userRepository.save(user);
+		return true;
+	}
 }
